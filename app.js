059609c89a55dfa2cee3d05dae1498d654b045bf
Kartik Singh <Kartik2598@gmail.com>
2020-08-25 02:43:48 +0530

    testing fasify

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 const app = express();
 const metatag = require('./routes/metatag-finder');
-app.use(cors())
 app.use(express.json())
 
 app.use('/api', metatag);
