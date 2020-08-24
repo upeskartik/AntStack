9b1cb610e07e1e3249d0ef33c6d64c9505f14e51
Kartik Singh <Kartik2598@gmail.com>
2020-08-25 03:34:37 +0530

    testing fasify

--- routes/metatag-finder.js
+++ routes/metatag-finder.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const request = require('request')
 const cheerio = require('cheerio')
+const parse5 = require('parse5')
 const dns = require('dns')
 
 router.get('/find-metatag', (req, res) => {
@@ -12,6 +13,8 @@ router.get('/find-metatag', (req, res) => {
         request(req.body.url, function (error, response, html) {
             if (!error && response.statusCode == 200) {
                 var $ = cheerio.load(html)
+                var doc = parse5.parse(html)
+                console.log(doc.childNodes)
                 let found = false
                 $('meta').each(function(i, e){
                     if ($(this).attr('name') == req.body.metatag_name){
@@ -38,14 +41,18 @@ router.get('/find-metatag', (req, res) => {
 })
 
 router.get('/dns-lookup', (req, res) => {
-    if (!req.body.url){
+    if (!req.body.url || !req.body.dns_txt){
         return res.status(400).send({ status: "ERROR", errorCode: "invalid-api-parameters" });
     }
     try {
         dns.resolve(req.body.url, rrtype=req.body.dns_txt, (err, address) => {
+            if (err){
+                res.status(400).send({ status: "ERROR", errorCode: ""})
+            }
             if (address){
                 res.send({
                     "status": "found",
+                    "dns_txt": req.body.dns_txt,
                     "domain": address
                 })
             } else{
